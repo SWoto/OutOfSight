2e54197c366f950488b0d8285d99e5d8670c6a4d
SWoto <26379593+SWoto@users.noreply.github.com>
2024-12-15 21:21:11 -0300

    changed schema

--- api/v1/endpoints/status.py
+++ api/v1/endpoints/status.py
@@ -20,11 +20,11 @@ async def get_status():
     current_date = datetime.now(timezone.utc)
     cpu_usage = psutil.cpu_percent()
     memory_usage = psutil.virtual_memory().percent
-    uptime = int((current_date - startup_time).total_seconds())
+    uptime_secs = int((current_date - startup_time).total_seconds())
     return {
         "status": "ok",
         "timestamp": current_date.isoformat(),
-        "uptime": uptime,
+        "uptime_secs": uptime_secs,
         "cpu_usage": cpu_usage,
         "memory_usage": memory_usage,
     }
--- schemas/status.py
+++ schemas/status.py
@@ -1,12 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PositiveFloat
 from typing import Literal
-from decimal import Decimal
 from datetime import datetime
 
 
 class PlainStatusSchema(BaseModel):
     status: Literal["ok", "not-ok"]
     timestamp: datetime
-    uptime: Decimal
-    cpu_usage: Decimal
-    memory_usage: Decimal
+    uptime_secs: int
+    cpu_usage: PositiveFloat
+    memory_usage: PositiveFloat
