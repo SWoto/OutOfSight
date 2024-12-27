96b540187d14cb509d966239f2ef081645d0e09b
SWoto <26379593+SWoto@users.noreply.github.com>
2024-12-27 15:00:16 -0300

    add logger

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
+import logging
 from datetime import datetime, timezone
 from fastapi import FastAPI
 import os
 
 from app.api.v1.api import api_router
+from app.core.logging import configure_logging
 
+logger = logging.getLogger(__name__)
 
 app = FastAPI(debug=True, title="OutOfSight")
 app.include_router(router=api_router)
