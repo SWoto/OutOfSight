e1875e743196a7730a2f70b0d4f9b3ab23c67df1
SWoto <26379593+SWoto@users.noreply.github.com>
2024-12-31 10:10:41 +0000

    simplify code for put user

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -42,21 +42,8 @@ async def get_user_by_id(id: UUID, db: Annotated[AsyncSession, Depends(get_db_se
 #TODO: Add administrator access to this
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED, response_model=ReturnUserSchema, tags=["Users"], summary="Update all user data", description="Get exing user and replace all of its data from provided information")
 async def put_user(id: UUID, user: PostPutUserSchema, db: Annotated[AsyncSession, Depends(get_db_session)]):
-    new_data = user.model_dump()
-    requested_user = await UsersModel.find_by_id(id, db)
-    if not requested_user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User does not exists")
-    
-    async with db as session:
-        requested_user = await UsersModel.find_by_id(id, session)
-        for key in new_data:
-            if key == 'password':
-                new_data[key] = get_hashed_password(new_data[key])
-            setattr(requested_user, key, new_data[key])
-        await session.commit()
-
-    new_requested_user = await UsersModel.find_by_id(id, db)
-    return new_requested_user
+    # Given that PostPutUserSchema is more restrictive then patch, first pass through PostPutUserSchema then sends to PatchUserSchema.
+    return await patch_user(id, user, db)
 
 
 #TODO: Add administrator access to this
