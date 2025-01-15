e3ceededfc3d9b563c4baf9bb17974e783c1909a
SWoto <26379593+SWoto@users.noreply.github.com>
2025-01-15 19:32:42 +0000

    orm

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,15 @@ import pytest
 from typing import Generator, AsyncGenerator
 from fastapi.testclient import TestClient
 from httpx import AsyncClient, ASGITransport
-from sqlalchemy import text
+from sqlalchemy import text, delete, and_
 
 import os
 os.environ["ENV_STATE"] = "test"
 
-from app.models.users import UsersModel
-from app.core.database import engine, create_database, create_tables, drop_tables, Session, initialize_default_values
-from app.main import app
 from app.core.configs import settings
+from app.main import app
+from app.core.database import engine, create_database, create_tables, drop_tables, Session, initialize_default_values, get_db_session
+from app.models import UsersModel, RolesModel, FilesModel
 
 
 @pytest.fixture(scope="session")
@@ -28,23 +28,37 @@ async def async_client(client) -> AsyncGenerator:
     async with AsyncClient(transport=ASGITransport(app=app), base_url=client.base_url) as ac:
         yield ac
 
+
 @pytest.fixture
 async def session() -> AsyncGenerator:
     async with Session() as session:
         yield session
+
 
 @pytest.fixture(scope="session", autouse=True)
 async def initiate_db() -> AsyncGenerator:
     await create_database(settings.DATABASE_URL, settings.POSTGRES_DB)
     await create_tables()
     await initialize_default_values()
-    yield 
+    yield
     await drop_tables()
 
 
 @pytest.fixture(autouse=True)
 async def clear_db() -> AsyncGenerator:
-    yield 
+    yield
     async with Session() as session:
-        await session.execute(text("DELETE FROM users"))  # Replace with UsersModel if ORM is preferred
+        await session.execute(delete(UsersModel))
+
+        await session.execute(
+            delete(RolesModel).where(
+                and_(
+                    RolesModel.authority > settings.MIN_ROLE, 
+                    RolesModel.authority < settings.MAX_ROLE
+                )
+            )
+        )
+
+        await session.execute(delete(FilesModel))
+
         await session.commit()
